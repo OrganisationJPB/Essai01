d292eca486a32794d8061786602e7de1700ab514
btssio <btssio@btssio-VirtualBox>
2014-09-11 16:03:37 +0200

    maj 3

--- src/uv1/Rectangle.java
+++ src/uv1/Rectangle.java
@@ -16,7 +16,7 @@ public class Rectangle {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("");// TODO code application logic here
     }
     
 }
