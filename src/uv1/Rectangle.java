721ce4ebfa005c6e37160a310c6394cb6bca7a7a
btssio <btssio@btssio-VirtualBox>
2014-09-11 15:47:50 +0200

    Revert "maj 2"
    This reverts commit db6d7523c4b3c172938a80658818dcb4584cdd72.

--- src/uv1/Rectangle.java
+++ src/uv1/Rectangle.java
@@ -16,9 +16,7 @@ public class Rectangle {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
-        System.out.println();
+        // TODO code application logic here
     }
     
 }
